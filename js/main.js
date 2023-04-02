b4e9872537885c4f62fb5d560eadb1f860a5bec4
Reche20 <91413674+Reche20@users.noreply.github.com>
2023-04-02 12:42:20 +0200

    1STOP

---
+++ js/main.js
@@ -0,0 +1,72 @@
+window.onload = function init() {
+
+  // Variables globales de utilidad
+var canvas = document.querySelector("canvas");
+var ctx=canvas.getContext("2d");
+var w = canvas.width;
+var h= canvas.height;
+
+// Game Framework
+var GF = function(){
+
+ var mainLoop = function(time){
+  // TU CÓDIGO AQUÍ
+  if(ctx){
+      canvas.width=canvas.width;
+      var X =getRandomInt(w);
+      var Y = getRandomInt(h);
+      var r = 5;
+      ctx.strokeStyle = "#000000";
+      ctx.fillStyle = "#FF0000";
+      ctx.lineWidth = 5;
+      ctx.arc(X,Y,r,0,2*Math.PI);
+      ctx.fill();
+      ctx.stroke();
+  }
+  //
+  requestAnimationFrame(mainLoop);
+
+};
+var start = function(){
+  requestAnimationFrame(mainLoop);
+};
+return {
+  start: start
+};
+};
+
+var game = new GF();
+game.start();
+
+
+test('Testeando colores', function(assert) {  
+
+  var done = assert.async();
+  var rojos = 0;
+ 
+ //ctx.fillStyle = 'red';
+ //ctx.fillRect(15,15,4,4);    
+
+setTimeout(function() {
+       var colores = [];
+       
+       colores.push(
+       Array.prototype.slice.apply(canvas.getContext("2d").getImageData(15, 15, 1, 1).data), Array.prototype.slice.apply(canvas.getContext("2d").getImageData(45, 45, 1, 1).data), Array.prototype.slice.apply(canvas.getContext("2d").getImageData(75, 75, 1, 1).data), Array.prototype.slice.apply(canvas.getContext("2d").getImageData(105, 105, 1, 1).data),
+Array.prototype.slice.apply(canvas.getContext("2d").getImageData(135, 135, 1, 1).data)
+       );
+       
+ for(var i=0; i< colores.length; i++)
+    if (colores[i][0] == 255)
+          rojos++;
+       
+ assert.ok( rojos >= 1, "Passed!");  
+  done();
+}, 10000 );
+  
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+}
